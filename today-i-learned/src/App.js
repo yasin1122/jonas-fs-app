4c683558eaf017f9541df5e70ccfc1d9a9c829f2
Yasin Cakal <whoisyasin@gmail.com>
2024-01-06 15:28:58 +0300

    passing props

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -88,7 +88,33 @@ function FactList() {
     <section>
       <ul className='fact-list'>
         {facts.map(fact => (
-          <li>Fact</li>
+          <li
+            key={fact.id}
+            className='fact'>
+            <p>
+              {fact.text}
+              <a
+                className='source'
+                href={fact.source}
+                target='_blank'>
+                (Source)
+              </a>
+            </p>
+            <span
+              className='tag'
+              style={{
+                backgroundColor: CATEGORIES.find(
+                  cat => cat.name === fact.category
+                ).color
+              }}>
+              {fact.category}
+            </span>
+            <div className='vote-buttons'>
+              <button>👍 {fact.votesInteresting}</button>
+              <button>🤯 {fact.votesMindblowing}</button>
+              <button>⛔️ {fact.votesFalse}</button>
+            </div>
+          </li>
         ))}
       </ul>
     </section>
