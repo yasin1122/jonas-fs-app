bb8ce5536a8f2fe473c70c482a3042ed48397405
Yasin Cakal <whoisyasin@gmail.com>
2024-01-06 15:45:22 +0300

    css edit

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -88,36 +88,42 @@ function FactList() {
     <section>
       <ul className='fact-list'>
         {facts.map(fact => (
-          <li
+          <Fact
             key={fact.id}
-            className='fact'>
-            <p>
-              {fact.text}
-              <a
-                className='source'
-                href={fact.source}
-                target='_blank'>
-                (Source)
-              </a>
-            </p>
-            <span
-              className='tag'
-              style={{
-                backgroundColor: CATEGORIES.find(
-                  cat => cat.name === fact.category
-                ).color
-              }}>
-              {fact.category}
-            </span>
-            <div className='vote-buttons'>
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔️ {fact.votesFalse}</button>
-            </div>
-          </li>
+            fact={fact}
+          />
         ))}
       </ul>
     </section>
+  )
+}
+
+function Fact({ fact }) {
+  return (
+    <li className='fact'>
+      <p>
+        {fact.text}
+        <a
+          className='source'
+          href={fact.source}
+          target='_blank'>
+          (Source)
+        </a>
+      </p>
+      <span
+        className='tag'
+        style={{
+          backgroundColor: CATEGORIES.find(cat => cat.name === fact.category)
+            .color
+        }}>
+        {fact.category}
+      </span>
+      <div className='vote-buttons'>
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
   )
 }
 
